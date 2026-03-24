26221372448d7cf950656fb938536ecec495ed89
ProshikS <129526743+ProshikS@users.noreply.github.com>
2026-03-24 05:19:55 +0500

    Add files via upload

--- lab4-task2/lab4-task2/Program.fs
+++ lab4-task2/lab4-task2/Program.fs
@@ -2,15 +2,26 @@
     | Node of string * Tree * Tree
     | Empty
 
-//вывод дерева
-let rec Print tree space = 
+    //вывод дерева
+let rec print tree space = //вывод дерева
     match tree with
-    | Node (data, left, right)
-        ->printfn "%sNode %s" space data
-          Print left (space + "\t")
-          Print right (space+ "\t")
+    | Node (value, left, right)
+        ->print right (space+ "\t")
+          printfn "%sNode %s" space value
+          print left (space + "\t")
     | Empty
         -> ()
+
+let rec insert value tree = //вставка
+    match tree with
+    |Empty -> Node(value, Empty, Empty)
+    |Node(v,left,right) -> 
+        if value < v then
+            Node(v, insert value left, right)
+        elif value > v then
+            Node(v, left, insert value right)
+        else
+            tree
 
 let rnd = System.Random() 
 
@@ -18,52 +29,51 @@ let rnd = System.Random()
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" 
 
 //создание случайной строки
-let rec RandString (str:string) (len:int) = 
+let rec randString (str:string) (len:int) = 
     if len = 0 then
         str
     else
         //случайный индекс от 0 до длины алфавита
         let randomIndex = rnd.Next(alphabet.Length)
         let randomChar = alphabet.[randomIndex].ToString()
-        RandString (str+randomChar) (len-1)
+        randString (str+randomChar) (len-1)
 
 //заполнение дерева
-let rec FillTree depth = 
-    if depth = 0 then Empty
+let rec fillTree n tree = //заполнение дерева
+    if n = 0 then
+        tree
     else
-        let value = RandString "" 4
-        let left = FillTree (depth - 1)
-        let right = FillTree (depth - 1)
-        Node(value, left, right)
+        let value = randString "" 4
+        fillTree (n - 1) (insert value tree)
 
 //рекурсивная функция свёртки
-let rec FoldTree f acc tree =
+let rec foldTree f acc tree =
     match tree with
     | Empty -> acc
     | Node (value, left, right) ->
-        let accLeft = FoldTree f acc left
-        let accRight = FoldTree f accLeft right
+        let accLeft = foldTree f acc left
+        let accRight = foldTree f accLeft right
         f accRight value
 
 //функция для проверки наличия элемента
-let Contains value tree =
-    let Folder acc nodeValue =
+let contains value tree =
+    let folder acc nodeValue =
         // если нашли, оставляем true
         if acc then 
             true        
         // иначе проверяем текущий узел
         else 
             nodeValue = value    
-    FoldTree Folder false tree
+    foldTree folder false tree
 
 [<EntryPoint>]
 let main argv =
-    let binTree = FillTree(4)
+    let binTree = fillTree 8 Empty
     printfn "Дерево:"
-    Print binTree ""
+    print binTree ""
     printf "Введите элемент: "
     let element = System.Console.ReadLine()
-    let res = Contains element binTree
+    let res = contains element binTree
     if res then
         printfn"Дерево содержит элемент %s" element
     else
